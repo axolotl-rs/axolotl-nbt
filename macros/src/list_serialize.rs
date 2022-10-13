181724b34b933f65353a6b26c2d70669d35ffe64
Wyatt Herkamp <wherkamp@kingtux.me>
2022-10-13 17:24:46 -0400

    ListSerialize implements deserialize_in_place

--- macros/src/list_serialize.rs
+++ macros/src/list_serialize.rs
@@ -21,6 +21,7 @@ pub(crate) fn parse_struct(type_ident: Ident, data: DataStruct) -> Result<TokenS
         ))?;
     };
     let visitor_name = format_ident!("{}Visitor", type_ident);
+    let visitor_in_place_name = format_ident!("{}InPlaceVisitor", type_ident);
     Ok(quote! {
         impl serde::Serialize for #type_ident {
             fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer{
@@ -52,9 +53,34 @@ pub(crate) fn parse_struct(type_ident: Ident, data: DataStruct) -> Result<TokenS
                 })
             }
         }
+        pub struct #visitor_in_place_name<'data>(&'data mut #type_ident);
+
+        impl<'de,'data> serde::de::Visitor<'de> for #visitor_in_place_name<'data> {
+            type Value = ();
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str(concat!("a sequence of ", #fields_size, " elements"))
+            }
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: serde::de::SeqAccess<'de> {
+                use serde::de::Error;
+                if let Some(size) = seq.size_hint() {
+                    if size != #fields_size {
+                        return Err(serde::de::Error::invalid_length(size, &self));
+                    }
+                }
+                #(
+                    self.0.#fields = seq.next_element()?.ok_or_else(|| A::Error::invalid_length(#fields_size, &self))?;
+                )*
+                Ok(())
+            }
+        }
+
+
         impl<'de> serde::Deserialize<'de> for #type_ident {
             fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
                 deserializer.deserialize_seq(#visitor_name)
+            }
+            fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error> where D: serde::Deserializer<'de> {
+                deserializer.deserialize_seq(#visitor_in_place_name(place))
             }
         }
 
